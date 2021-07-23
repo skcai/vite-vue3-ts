1603c9da7ce12939824b4243011fcb4425b185d6
Kangle Cai <kanglecai@computime.com>
2021-07-23 17:30:14 +0800

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/vite-vue3-ts/',
   server: {
     host: '0.0.0.0',
   }
