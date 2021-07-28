0d0bb79821ea91861faf9f52262a12e1d5a6f4aa
skcai <304884568@qq.com>
2021-07-28 22:15:28 +0800

    fix the Prettier and ESLint conflict

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,19 +4,12 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    'standard'
-  ],
+  extends: ['plugin:vue/essential', 'standard', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 12,
     parser: '@typescript-eslint/parser',
     sourceType: 'module'
   },
-  plugins: [
-    'vue',
-    '@typescript-eslint'
-  ],
-  rules: {
-  }
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {}
 }
